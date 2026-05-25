c5cd42c08b95390a1b83bbb9be5d73a6255e182c
Soumya-Bhagat~ <soumyabhagat31@gmail.com>
2026-05-25 10:33:56 +0530

    Frontend deploy

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -9,7 +9,8 @@ export interface Game {
   gameOver: boolean;
 }
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL =
+  import.meta.env.VITE_API_URL;
 
 export async function createGame(): Promise<Game> {
   const response = await fetch(
